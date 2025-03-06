7480b745f65697a036242b2ebde31921ead37134
psithepyro <103456248+psithepyro@users.noreply.github.com>
2025-03-05 22:50:08 -0800

    added a fetch function to the script.js file

--- my-project/script.js
+++ my-project/script.js
@@ -1,1 +1,21 @@
 // To-Do
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOGVmNjBjZjY1Njg0Njc1ZWIyMmIxYjJkZWJjM2E3MSIsIm5iZiI6MTc0MDg2NTQxNi4zOTIsInN1YiI6IjY3YzM3Zjg4NjQ2NmJkOGVkMjRkZmZlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cL8f5aPbrZen1z0Z6Pm-cCSHZXW7o5vaReq4e-Bmar0' 
+    }
+};
+
+async function fetchMovies() {
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/movie/popular', options);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }   
+}
+
+fetchMovies();
